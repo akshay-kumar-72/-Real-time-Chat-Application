e5633e61baa1af715bd29450e96d498b918ee5ee
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-10 08:28:00 -0500

    Update server.js

--- Real-time Chat App/server.js
+++ Real-time Chat App/server.js
@@ -1,1 +1,51 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const Message = require("./models/Message");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// Socket.IO Events
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("sendMessage", async (data) => {
+    const message = new Message(data);
+    await message.save();
+
+    io.to(data.room).emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: " + socket.id);
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
