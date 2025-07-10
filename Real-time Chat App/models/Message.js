d227ca5ffb3ffbaeccab7c6402ef486cea897fda
akshay-kumar-72 <akshaykumar725296@gmail.com>
2025-07-10 08:32:01 -0500

    Update Message.js

--- Real-time Chat App/models/Message.js
+++ Real-time Chat App/models/Message.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const messageSchema = new mongoose.Schema({
+  sender: String,
+  content: String,
+  room: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
